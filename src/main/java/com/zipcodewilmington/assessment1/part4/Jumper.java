e9cfa3ab94fa3788d0de2be0e4e8a77d8edcd1ab
Nyoxide <nyoxidetwitter@gmail.com>
2020-02-17 10:52:45 -0500

    finished assessment with 68 passing tests, and 2 that crash due to poor implementation

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -5,7 +5,17 @@ public class Jumper {
     /*
      * Complete the function below.
      */
-    public int jumps(int k, int j) {
-        return -1;
+    public int jumps(int flagHeight, int jumpHeight) {
+        int currentHeight = 0;
+        int jumps = 0;
+        while (currentHeight != flagHeight) {
+            currentHeight += jumpHeight;
+            if (currentHeight > flagHeight) {
+                currentHeight -= jumpHeight;
+                currentHeight++;
+            }
+            jumps++;
+        }
+        return jumps;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,31 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
+import java.util.ArrayList;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        ArrayList<String> substrings = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            for (int k = 1; k <= input.length() - i; k++) {
+                substrings.add(input.substring(i, i+k));
+            }
+        }
+        int palindromes = 0;
+        for (String s : substrings) {
+            if (isPalindrome(s)) {
+                palindromes++;
+            }
+        }
+        return palindromes;
+    }
+
+    public Boolean isPalindrome(String s) {
+        if (BasicStringUtils.reverse(s).equals(s)) {
+            return true;
+        }
+        return false;
     }
 }
