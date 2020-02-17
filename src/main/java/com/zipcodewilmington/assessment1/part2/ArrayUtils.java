c65f057c8ff449e9d48dd19bf304e59934cb2205
Nyoxide <nyoxidetwitter@gmail.com>
2020-02-17 11:21:36 -0500

    finished assessment with 70/70 passing tests :)

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -31,13 +32,22 @@ public class ArrayUtils {
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
     public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        Object[] toRet = new Object[objectArray.length - 1];
-        for (int i = 0; i < objectArray.length; i++) {
-            if (!objectArray[i].equals(objectToRemove)) {
-                toRet[i] = objectArray[i];
+
+        ArrayList<Object> objs = new ArrayList<>();
+        for (Object o : objectArray) {
+            if (!o.equals(objectToRemove)) {
+                objs.add(o);
             }
         }
-        return toRet;
+
+
+
+        Object[] toRet = new Object[objs.size()];
+        for (int i = 0; i < toRet.length; i++) {
+            toRet[i] = objs.get(i);
+        }
+
+        return tryToFixDumbCasting(toRet);
     }
 
     /**
@@ -100,6 +110,29 @@ public class ArrayUtils {
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
     public static Object[] mergeArrays(Object[] objectArray, Object[] objectArrayToAdd) {
-        return null;
+        Object[] toRet = new Object[objectArray.length + objectArrayToAdd.length];
+        int newIndex = 0;
+        for (int i = 0; i < objectArray.length; i++, newIndex++) {
+            toRet[newIndex] = objectArray[i];
+        }
+
+        for (int i = 0; i < objectArrayToAdd.length; i++, newIndex++) {
+            toRet[newIndex] = objectArrayToAdd[i];
+        }
+
+        return tryToFixDumbCasting(toRet);
+    }
+
+    private static Object[] tryToFixDumbCasting(Object[] toRet) {
+        if (toRet.length > 0 && toRet[0] instanceof Integer) {
+            Integer[] arr = new Integer[toRet.length];
+            for (int i = 0; i < toRet.length; i++) {
+                if (toRet[i] instanceof Integer && toRet[i] != null) {
+                    arr[i] = (Integer) toRet[i];
+                }
+            }
+            return arr;
+        }
+        return toRet;
     }
 }
